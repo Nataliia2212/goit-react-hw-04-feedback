4ba77f80ca32092a2fa871ec5c1e15bc59f20f05
Nataliia Plokhotnyk <shmilo.nataliya@gmail.com>
2024-02-23 19:03:34 +0200

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,57 +1,54 @@
-import FeedbackOptions from "./FeedbackOptions/FeedbackOptions";
-import React, { Component } from 'react';
-import Statistics from "./Statistics/Statistics";
-import Section from "./Section/Section";
-import Notification from "./Notification/Notification";
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Statistics from './Statistics/Statistics';
+import Section from './Section/Section';
+import Notification from './Notification/Notification';
+import React from 'react';
+import { useState } from 'react';
 
-export default class App extends Component {
-   state = {
+export default function App() {
+    const [state, setState] = useState({
         good: 0,
         neutral: 0,
-        bad: 0
-    }
+        bad: 0,
+    });
+    const { good, bad, neutral } = state;
 
-    handleLeaveFeedback = (e) => {
-      const name = e.target.name;
-        this.setState((prevState) => {
-          return {
-            [name] : prevState[name] + 1
-          }
-        });
-    }
+    const handleLeaveFeedback = e => {
+        const name = e.target.name;
+        setState(prev => ({ ...prev, [name]: prev[name] + 1 }));
+    };
 
-    countTotalFeedback = ()=> {
-      const { good, neutral, bad } = this.state;
-        return good + neutral + bad
-    }
+    const countTotalFeedback = () => {
+        return good + neutral + bad;
+    };
 
-    countPositiveFeedbackPercentage = () => {
-        const { good} = this.state;
-        return Math.ceil(good / this.countTotalFeedback() * 100)
-    }
+    const countPositiveFeedbackPercentage = () => {
+        return Math.ceil((good / countTotalFeedback()) * 100);
+    };
 
-  render() {
-    const {good, neutral, bad} = this.state
     return (
-      <>
-        <h1>Expresso</h1>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={Object.keys(this.state)}
-            onLeaveFeedback={this.handleLeaveFeedback}>
-          </FeedbackOptions>
-        </Section>
-        
-        <Section title="Statistics">
-         { !this.countTotalFeedback() ? <Notification message="There is no feedback"/> :
-          <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            total={this.countTotalFeedback}
-            positivePercentage={this.countPositiveFeedbackPercentage} />}
-        </Section>
-      </>
-    )
-  }
+        <>
+            <h1>Expresso</h1>
+            <Section title="Please leave feedback">
+                <FeedbackOptions
+                    options={Object.keys(state)}
+                    onLeaveFeedback={handleLeaveFeedback}
+                ></FeedbackOptions>
+            </Section>
+
+            <Section title="Statistics">
+                {!countTotalFeedback() ? (
+                    <Notification message="There is no feedback" />
+                ) : (
+                    <Statistics
+                        good={good}
+                        neutral={neutral}
+                        bad={bad}
+                        total={countTotalFeedback}
+                        positivePercentage={countPositiveFeedbackPercentage}
+                    />
+                )}
+            </Section>
+        </>
+    );
 }
--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
-import css from './FeedbackOptions.module.css'
+import css from './FeedbackOptions.module.css';
 
 export default function FeedbackOptions({ options, onLeaveFeedback }) {
-  return ( <ul className={css.list}>
-    {options.map(option =>
-      <li key={option}>
-          <button className={css.button} onClick={onLeaveFeedback} type='button' name={option}>{option}</button>
-      </li>
-      )}   
-    </ul>
-  )
+    return (
+        <ul className={css.list}>
+            {options.map(option => (
+                <li key={option}>
+                    <button
+                        className={css.button}
+                        onClick={onLeaveFeedback}
+                        type="button"
+                        name={option}
+                    >
+                        {option}
+                    </button>
+                </li>
+            ))}
+        </ul>
+    );
 }
